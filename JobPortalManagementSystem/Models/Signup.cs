5846d1b837c1996893780a9f587d43654a7c20c0
Devika <devikac7499@gmail.com>
2023-07-24 22:36:24 +0530

    Add project files.

---
+++ JobPortalManagementSystem/Models/Signup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace JobPortalManagementSystem.Models
+{
+    public class Signup
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last name")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Please enter your date of birth.")]
+        public DateTime dateOfBirth { get; set; }
+
+        [Display(Name = "Gender")]
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "Gender must be a single character.")]
+        [Required(ErrorMessage = "Please enter your gender.")]
+        public char gender { get; set; }
+
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Required(ErrorMessage = "Please enter your email address.")]
+        public string email { get; set; }
+
+        [Display(Name = "Phone")]
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        public string phone { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Please enter your address.")]
+        public string address { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Please enter your state.")]
+        public string state { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Please enter your city.")]
+        public string city { get; set; }
+
+        [Display(Name = "Pincode")]
+        [Required(ErrorMessage = "Please enter your pincode.")]
+        public int pincode { get; set; }
+
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Please enter your country.")]
+        public string country { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please enter your username.")]
+        public string username { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please enter your password.")]
+        public byte[] password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please confirm your password.")]
+        public string confirmPassword { get; set; }
+    }
+
+}
---
+++ JobPortalManagementSystem/Repository/SignupRepository.cs
@@ -0,0 +1,156 @@
+﻿using JobPortalManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace JobPortalManagementSystem.Repository
+{
+    public class SignupRepository
+    {
+        private SqlConnection connection;
+
+        /// <summary>
+        /// Handle connection
+        /// </summary>
+        private void Connection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["GetDatabaseConnection"].ToString();
+            connection = new SqlConnection(connectionString);
+        }
+        //Model name
+        public bool AddDetails(Signup signup)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPI_Signup", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@firstName", signup.firstName);
+            command.Parameters.AddWithValue("@lastName", signup.lastName);
+            command.Parameters.AddWithValue("@dateOfBirth", signup.dateOfBirth);
+            command.Parameters.AddWithValue("@gender", signup.gender);
+            command.Parameters.AddWithValue("@email", signup.email);
+            command.Parameters.AddWithValue("phone", signup.phone);
+            command.Parameters.AddWithValue("@address", signup.address);
+            command.Parameters.AddWithValue("@city", signup.city);
+            command.Parameters.AddWithValue("@state", signup.state);
+            command.Parameters.AddWithValue("@pincode", signup.pincode);
+            command.Parameters.AddWithValue("@country", signup.country);
+            command.Parameters.AddWithValue("@username", signup.username);
+            command.Parameters.AddWithValue("@password", signup.password);
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Signup> GetDetails()
+        {
+            Connection();
+            List<Signup> SignupList = new List<Signup>();
+            SqlCommand command = new SqlCommand("SPI_Signup", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            connection.Open();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+
+            foreach (DataRow row in dataTable.Rows)
+                SignupList.Add(new Signup
+                {
+                    Id = Convert.ToInt32(row["ID"]),
+                    firstName = Convert.ToString(row["firstName"]),
+                    lastName = Convert.ToString(row["lastName"]),
+                    dateOfBirth = Convert.ToDateTime(row["dateOfBirth"]),
+                    gender = Convert.ToChar(row["gender"]),
+                    email = Convert.ToString(row["email"]),
+                    phone = Convert.ToString(row["phone"]),
+                    address = Convert.ToString(row["address"]),
+                    city = Convert.ToString(row["city"]),
+                    state = Convert.ToString(row["state"]),
+                    pincode = Convert.ToInt32(row["pincode"]),
+                    country = Convert.ToString(row["country"]),
+                    username = Convert.ToString(row["username"]),
+                    password = (byte[])(row["password"])
+                }
+                );
+                return SignupList;
+        }
+
+        public bool EditDetails(Signup signup)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPU_Signup", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Id", signup.Id);
+            command.Parameters.AddWithValue("@firstName", signup.firstName);
+            command.Parameters.AddWithValue("@lastName", signup.lastName);
+            command.Parameters.AddWithValue("@dateOfBirth", signup.dateOfBirth);
+            command.Parameters.AddWithValue("@gender", signup.gender);
+            command.Parameters.AddWithValue("@email", signup.email);
+            command.Parameters.AddWithValue("phone", signup.phone);
+            command.Parameters.AddWithValue("@address", signup.address);
+            command.Parameters.AddWithValue("@city", signup.city);
+            command.Parameters.AddWithValue("@state", signup.state);
+            command.Parameters.AddWithValue("@pincode", signup.pincode);
+            command.Parameters.AddWithValue("@country", signup.country);
+            command.Parameters.AddWithValue("@username", signup.username);
+            command.Parameters.AddWithValue("@password", signup.password);
+
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool DeleteDetails(int Id)
+        {
+            Connection();
+            SqlCommand command = new SqlCommand("SPD_Signup", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("Id",Id);
+
+            connection.Open();
+            int i = command.ExecuteNonQuery();
+            connection.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+    }
+}
